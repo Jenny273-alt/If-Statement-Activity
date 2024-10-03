640e8f7bd2db232367f9a03e57411f816eaa4169
Jenny273-alt <naungayanjenny90@gmail.com>
2024-10-03 10:02:54 +0700

    ageCheck.js

---
+++ ageCheck.js
@@ -0,0 +1,13 @@
+let age = prompt("Enter your age:");
+
+// Convert the age to a number
+age = Number(age);
+
+if (age < 13) {
+  console.log("You are a child.");
+} else if (age < 20) {
+  console.log("You are a teenager.");
+} else {
+  console.log("You are an adult.");
+}
+//https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.w3schools.com%2Fjs%2Fjs_if_else.asp%3Ffbclid%3DIwZXh0bgNhZW0CMTAAAR158GiRkjSIWGmd_GFcKphLnxgTocMG8kXjY7DrOjZUJMt2ARYRnz-5OSY_aem_GBVf-oJNnXWovhELtAQNOA&h=AT2xCctkMWnkFVePGDbdDI68qIKYVqGlYQRrtrM914zDhbthXGeupfHLh-Rv50u2LksU3knMEMIC_0y9RJAqc6MAYnEqvgie8bAQFY4LgXvfGOhJH0Gl-IPDoZ5r69-J8TZMRA
